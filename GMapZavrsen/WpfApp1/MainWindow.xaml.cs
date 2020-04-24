88ce7ca3aad335a08406df71a673d88d4e2ecacf
aleksandar-r <aleksandar.h@outlook.com>
2020-04-24 09:24:37 +0200

    Base

---
+++ GMapZavrsen/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using WpfApp1.Model;
+using Brushes = System.Drawing.Brushes;
+using Pen = System.Drawing.Pen;
+using Point = WpfApp1.Model.Point;
+using Size = System.Drawing.Size;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double noviX, noviY;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GMapProvider.WebProxy = WebRequest.GetSystemWebProxy();
+            GMapProvider.WebProxy.Credentials = CredentialCache.DefaultNetworkCredentials;
+            gmap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            //gmap.SetPositionByKeywords("Novi Sad, Serbia");
+            gmap.Position = new GMap.NET.PointLatLng(45.254765, 19.844184);
+            gmap.ShowCenter = false;
+        }
+
+		private void LoadButton_Click(object sender, RoutedEventArgs e)
+		{
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("Geographic.xml");
+
+            XmlNodeList nodeList;
+
+            GMap.NET.WindowsForms.GMapOverlay subsOverlay = new GMap.NET.WindowsForms.GMapOverlay("SubstationsOverlay");
+
+            SubstationEntity sub = new SubstationEntity();
+
+            nodeList = xmlDoc.DocumentElement.SelectNodes("/NetworkModel/Substations/SubstationEntity");
+            foreach (XmlNode node in nodeList)
+            {
+                sub.Id = long.Parse(node.SelectSingleNode("Id").InnerText);
+                sub.Name = node.SelectSingleNode("Name").InnerText;
+                sub.X = double.Parse(node.SelectSingleNode("X").InnerText);
+                sub.Y = double.Parse(node.SelectSingleNode("Y").InnerText);
+
+                ToLatLon(sub.X, sub.Y, 34, out noviX, out noviY);
+                GMapMarker marker = new GMarkerGoogle(new PointLatLng(noviX, noviY), GMarkerGoogleType.blue_pushpin);
+                marker.ToolTipText = "Substation\nID: " + sub.Id + "  Name: " + sub.Name;
+                marker.ToolTip.Fill = Brushes.Black;
+                marker.ToolTip.Foreground = Brushes.White;
+                marker.ToolTip.Stroke = Pens.Black;
+                marker.ToolTip.TextPadding = new Size(20, 20);
+
+                subsOverlay.Markers.Add(marker);
+            }
+
+
+            GMapOverlay routes = new GMapOverlay("routes");
+            List<PointLatLng> points = new List<PointLatLng>();
+
+
+            LineEntity l = new LineEntity();
+
+            nodeList = xmlDoc.DocumentElement.SelectNodes("/NetworkModel/Lines/LineEntity");
+            foreach (XmlNode node in nodeList)
+            {
+                l.Id = long.Parse(node.SelectSingleNode("Id").InnerText);
+                l.Name = node.SelectSingleNode("Name").InnerText;
+                if (node.SelectSingleNode("IsUnderground").InnerText.Equals("true"))
+                {
+                    l.IsUnderground = true;
+                }
+                else
+                {
+                    l.IsUnderground = false;
+                }
+                l.R = float.Parse(node.SelectSingleNode("R").InnerText);
+                l.ConductorMaterial = node.SelectSingleNode("ConductorMaterial").InnerText;
+                l.LineType = node.SelectSingleNode("LineType").InnerText;
+                l.ThermalConstantHeat = long.Parse(node.SelectSingleNode("ThermalConstantHeat").InnerText);
+                l.FirstEnd = long.Parse(node.SelectSingleNode("FirstEnd").InnerText);
+                l.SecondEnd = long.Parse(node.SelectSingleNode("SecondEnd").InnerText);
+
+                foreach (XmlNode pointNode in node.ChildNodes[9].ChildNodes) // 9 posto je Vertices 9. node u jednom line objektu
+                {
+                    Point p = new Point();
+
+                    p.X = double.Parse(pointNode.SelectSingleNode("X").InnerText);
+                    p.Y = double.Parse(pointNode.SelectSingleNode("Y").InnerText);
+
+                    ToLatLon(p.X, p.Y, 34, out noviX, out noviY);
+
+                    points.Add(new PointLatLng(noviX, noviY));
+                }
+
+                GMapRoute route = new GMapRoute(points, "Line: " + l.Id + "\n" + l.Name);
+                route.Stroke = new Pen(Brushes.DarkBlue, 2);
+                routes.Routes.Add(route);
+
+                points.Clear();
+            }
+
+            gmap.Overlays.Add(routes);
+            gmap.Overlays.Add(subsOverlay);
+        }
+
+		//From UTM to Latitude and longitude in decimal
+		public static void ToLatLon(double utmX, double utmY, int zoneUTM, out double latitude, out double longitude)
+		{
+			bool isNorthHemisphere = true;
+
+			var diflat = -0.00066286966871111111111111111111111111;
+			var diflon = -0.0003868060578;
+
+			var zone = zoneUTM;
+			var c_sa = 6378137.000000;
+			var c_sb = 6356752.314245;
+			var e2 = Math.Pow((Math.Pow(c_sa, 2) - Math.Pow(c_sb, 2)), 0.5) / c_sb;
+			var e2cuadrada = Math.Pow(e2, 2);
+			var c = Math.Pow(c_sa, 2) / c_sb;
+			var x = utmX - 500000;
+			var y = isNorthHemisphere ? utmY : utmY - 10000000;
+
+			var s = ((zone * 6.0) - 183.0);
+			var lat = y / (c_sa * 0.9996);
+			var v = (c / Math.Pow(1 + (e2cuadrada * Math.Pow(Math.Cos(lat), 2)), 0.5)) * 0.9996;
+			var a = x / v;
+			var a1 = Math.Sin(2 * lat);
+			var a2 = a1 * Math.Pow((Math.Cos(lat)), 2);
+			var j2 = lat + (a1 / 2.0);
+			var j4 = ((3 * j2) + a2) / 4.0;
+			var j6 = ((5 * j4) + Math.Pow(a2 * (Math.Cos(lat)), 2)) / 3.0;
+			var alfa = (3.0 / 4.0) * e2cuadrada;
+			var beta = (5.0 / 3.0) * Math.Pow(alfa, 2);
+			var gama = (35.0 / 27.0) * Math.Pow(alfa, 3);
+			var bm = 0.9996 * c * (lat - alfa * j2 + beta * j4 - gama * j6);
+			var b = (y - bm) / v;
+			var epsi = ((e2cuadrada * Math.Pow(a, 2)) / 2.0) * Math.Pow((Math.Cos(lat)), 2);
+			var eps = a * (1 - (epsi / 3.0));
+			var nab = (b * (1 - epsi)) + lat;
+			var senoheps = (Math.Exp(eps) - Math.Exp(-eps)) / 2.0;
+			var delt = Math.Atan(senoheps / (Math.Cos(nab)));
+			var tao = Math.Atan(Math.Cos(delt) * Math.Tan(nab));
+
+			longitude = ((delt * (180.0 / Math.PI)) + s) + diflon;
+			latitude = ((lat + (1 + e2cuadrada * Math.Pow(Math.Cos(lat), 2) - (3.0 / 2.0) * e2cuadrada * Math.Sin(lat) * Math.Cos(lat) * (tao - lat)) * (tao - lat)) * (180.0 / Math.PI)) + diflat;
+		}
+	}
+}
